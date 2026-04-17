715cb44851898eec653c091008047b6d5a1708e3
mmsqe <tqd0800210105@gmail.com>
2026-04-17 19:35:30 +0800

    serialize Address at API

--- pydefi/aggregator/openocean.py
+++ pydefi/aggregator/openocean.py
@@ -135,8 +135,8 @@ class OpenOcean(BaseAggregator):
         if "gasPrice" not in kwargs:
             kwargs = {**kwargs, "gasPrice": await self._get_gas_price()}
         params: dict[str, Any] = {
-            "inTokenAddress": amount_in.token.address,
-            "outTokenAddress": token_out.address,
+            "inTokenAddress": amount_in.token.encoded_address,
+            "outTokenAddress": token_out.encoded_address,
             "amount": str(amount_in.human_amount),
             "slippage": str(self._slippage_to_percent(slippage_bps)),
             **kwargs,
@@ -190,8 +190,8 @@ class OpenOcean(BaseAggregator):
         if "gasPrice" not in kwargs:
             kwargs = {**kwargs, "gasPrice": await self._get_gas_price()}
         params: dict[str, Any] = {
-            "inTokenAddress": amount_in.token.address,
-            "outTokenAddress": token_out.address,
+            "inTokenAddress": amount_in.token.encoded_address,
+            "outTokenAddress": token_out.encoded_address,
             "amount": str(amount_in.human_amount),
             "slippage": str(self._slippage_to_percent(slippage_bps)),
             "account": from_address,
